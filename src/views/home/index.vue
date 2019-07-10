cd4e0b8bea7e7277de10d9e52e949ccec44c3dff
hanmiaomiao <18531151201@163.com>
2019-07-10 21:46:53 +0800

    首页-页面布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,13 +1,109 @@
 <template>
-    <div>Home 页面</div>
+    <div>
+     <!-- 头部 -->
+      <van-nav-bar
+            title="首页"
+          />
+           <!-- /头部 -->
+          <!-- 频道标签 -->
+          <van-tabs class="channel-tabs" v-model="activeChannelIndex">
+            <van-tab title="标签 1">
+               <!-- 下拉刷新组件
+            isLoading 控制下拉的loading状态
+            refresh  下拉之后出发的事件
+             -->
+             <van-pull-refresh v-model="pullRefreshLoading" @refresh="onRefresh">
+             <!--
+               loading 控制加载更多的loading 状态
+               finished 控制是否已经加载结束
+               onload 事件会在滚动到底部区域的时候自动调用，每次onload他会自动让 loading 为 true
+              -->
+              <!-- 标签 -->
+              <van-list
+                  v-model="loading"
+                  :finished="finished"
+                  finished-text="没有更多了"
+                  @load="onLoad"
+                >
+                <!-- 列表中的内容 -->
+                  <van-cell
+                    v-for="item in list"
+                    :key="item"
+                    :title="item"
+                  />
+                </van-list>
+                 </van-pull-refresh>
+            </van-tab>
+            <van-tab title="标签 2">内容 2</van-tab>
+            <van-tab title="标签 3">内容 3</van-tab>
+            <van-tab title="标签 4">内容 4</van-tab>
+            <van-tab title="标签 5">内容 5</van-tab>
+            <van-tab title="标签 6">内容 6</van-tab>
+            <van-tab title="标签 7">内容 7</van-tab>
+            <van-tab title="标签 8">内容 8</van-tab>
+          </van-tabs>
+          <!-- /频道标签 -->
+          <!-- 底部导航 -->
+          <van-tabbar route>
+            <van-tabbar-item icon="home-o" to="/">首页</van-tabbar-item>
+            <van-tabbar-item icon="comment-o" to="wd">问答</van-tabbar-item>
+            <van-tabbar-item icon="cashier-o" to="sp">视频</van-tabbar-item>
+            <van-tabbar-item icon="user-o" to="my">我的</van-tabbar-item>
+          </van-tabbar>
+            <!-- /底部导航 -->
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'HomeIndex'
+  name: 'HomeIndex',
+  data () {
+    return {
+      activeChannelIndex: 0,
+      list: [],
+      loading: false,
+      finished: false,
+      pullRefreshLoading: false
+    }
+  },
+  methods: {
+    onLoad () {
+      // 异步更新数据
+      setTimeout(() => {
+        for (let i = 0; i < 10; i++) {
+          this.list.push(this.list.length + 1)
+        }
+        // 加载状态结束
+        this.loading = false
+
+        // 数据全部加载完成
+        if (this.list.length >= 40) {
+          // 所有数据加载完毕，设置finished为true
+          this.finished = true
+        }
+      }, 500)
+    },
+    onRefresh () {
+      setTimeout(() => {
+        this.pullRefreshLoading = false
+      }, 500)
+    }
+  }
 }
 </script>
 
 <style lang="less" scope>
+.channel-tabs {
+  margin-bottom: 100px;
+}
 
+// /deep/ 的作用（深度作用选择器）
+// 注意：我们在页面上测量的是设备的像素，我们的样式规则转换是基于75 进行转换的，所以在这里写的都 乘以 2
+.channel-tabs /deep/ .van-tabs__wrap {
+  position: fixed;
+  top: 92px;
+}
+.channel-tabs /deep/ .van-tabs__content {
+  margin-top: 92px;
+}
 </style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -13,6 +13,7 @@
                   clearable
                   label="手机号"
                   placeholder="请输入手机号"
+                  :error-message="errors.mobile"
                 />
                <van-cell-group>
                   <van-field
@@ -21,8 +22,12 @@
                     clearable
                     label="短信验证码"
                     placeholder="请输入短信验证码"
+                   :error-message="errors.code"
                   >
-                    <van-button slot="button" size="small" type="primary">发送验证码</van-button>
+                    <van-button
+                    slot="button"
+                    size="small"
+                    type="primary">发送验证码</van-button>
                   </van-field>
                 </van-cell-group>
               </van-cell-group>
@@ -49,14 +54,33 @@ export default {
         mobile: '18531151201',
         code: '123456'
       },
-      loginLoading: false // 控制登录按钮的 loading 状态
+      loginLoading: false, // 控制登录按钮的 loading 状态
+      errors: {
+        mobile: '',
+        code: ''
+      }
     }
   },
   methods: {
     async handleLogin () {
-      this.loginLoading = true
       try {
-        // 登陆成功后要做的事情
+        // 发送请求之前，校验表单数据，校验通过，才能进行登录
+        const { mobile, code } = this.user
+        const errors = this.errors
+        if (mobile.length) {
+          errors.mobile = ''
+        } else {
+          errors.mobile = '手机号不能为空'
+          return
+        }
+        if (code.length) {
+          errors.code = ''
+        } else {
+          errors.code = '验证码不能为空'
+          return
+        }
+        // 表单验证通过后，发送请求，loading加载
+        this.loginLoading = true
         const data = await login(this.user)
         // 登录成功后，提交mutation完成对状态的修改
         this.$store.commit('setUser', data)
@@ -72,6 +96,7 @@ export default {
       }
       this.loginLoading = false
     }
+
   }
 }
 </script>
